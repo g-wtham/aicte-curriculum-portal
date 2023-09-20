8db521a2f12484b9ea745b5e6ce089c98f82242c
Gowtham <106096598+g-wtham@users.noreply.github.com>
2023-09-20 11:55:48 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAAqCb80lIC0-Dp6buiUALNk9AL406epd4",
+      authDomain: "aicte-curriculum.firebaseapp.com",
+      projectId: "aicte-curriculum",
+      storageBucket: "aicte-curriculum.appspot.com",
+      messagingSenderId: "354668287037",
+      appId: "1:354668287037:web:65c067b3fae53aa672c654",
+      measurementId: "G-QLZ5HNE30Q"
+};
